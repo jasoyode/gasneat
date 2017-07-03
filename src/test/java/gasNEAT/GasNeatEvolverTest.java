94519cf1a6b6e3c6aedded9a9b0e2dfd3b2a26d3
jasoyode <jasoyode@indiana.edu>
2017-07-02 20:30:33 -0400

    adding 1 evolver test in

--- src/test/java/gasNEAT/GasNeatEvolverTest.java
+++ src/test/java/gasNEAT/GasNeatEvolverTest.java
@@ -50,8 +50,11 @@ public GasNeatEvolverTest( String name ) {
 	
 	experimentsToRun = new ArrayList<String>();
 	
-	/*
+	
 	experimentsToRun.add("experiments/sanity_tests/gas_act_mod/task_gas_act_mod.properties");
+	
+	/*
+	
 	experimentsToRun.add("experiments/sanity_tests/gas_activated_neurons/task_gas_activated_neurons.properties");
 	experimentsToRun.add("experiments/sanity_tests/multi_output/multi_output.properties");
 	experimentsToRun.add("experiments/sanity_tests/multi_output_delayed/multi_output_delayed.properties");
